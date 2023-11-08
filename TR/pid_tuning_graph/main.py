466531ced747f297e02085e4481c076cd7dbddb2
Alvin <alvinng2707@gmail.com>
2023-11-08 15:48:01 +0800

    Graph now shows average value

--- TR/pid_tuning_graph/main.py
+++ TR/pid_tuning_graph/main.py
@@ -11,28 +11,33 @@ def get_args():
         exit()
 
 def main():
-    #port, target_rpm = get_args()
-    target_rpm = 500.0
+    port, target_rpm = get_args()
 
     data = []
+    average_values = []
 
     data_size = 5000
 
-    serial_port = serial.Serial(port="COM3", baudrate=115200, bytesize=8, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
+    serial_port = serial.Serial(port=port, baudrate=115200, bytesize=8, parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
     serial_string = ""
     while len(data) < data_size:
         if (serial_port.in_waiting > 0):
             serial_string = serial_port.readline()
             try:
                 data.append(float(serial_string.decode().strip()))
+                if (data[-1] >= target_rpm - 6):
+                    average_values.append(data[-1])
+                print(len(data))
             except:
                 data_size -= 1
-            #print(serial_string.decode().strip())
 
     print(f"Received data: {len(data)}")
     print(data)
 
     plt.axhline(y=target_rpm, color='r')
+    plt.axhline(y=sum(average_values)/len(average_values), color='g', linestyle='--')
+    print(F"Average: {sum(average_values)/len(average_values)}")
+
     plt.plot(np.array(data))
 
     plt.show()
