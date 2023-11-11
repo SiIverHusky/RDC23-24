253a157fe65644dcd7e2282285b2344dfb19c9bc
Alvin <alvinng2707@gmail.com>
2023-11-11 16:42:12 +0800

    Code should work now

--- TR/sw-tutorial/Core/Src/main.c
+++ TR/sw-tutorial/Core/Src/main.c
@@ -105,9 +105,7 @@ int main(void) {
     MX_GPIO_Init();
     MX_CAN1_Init();
     MX_CAN2_Init();
-    MX_SPI1_Init();
     MX_USART1_UART_Init();
-    MX_I2C2_Init();
     MX_DMA_Init();
     MX_USART2_UART_Init();
     MX_TIM5_Init();
